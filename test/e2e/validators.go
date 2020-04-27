45a48a8805085b021b6885baac33180e46d305fe
Otávio Fernandes <otaviof@gmail.com>
2020-04-27 15:57:54 +0200

    External Container Registry for E2E (#121)
    * Project infrastructure updates.
    
    * Updating developer's documentation.
    
    * External Container Registry
    
    For external container registry it also needs a
    `kubernetes.io/dockerconfigjson` secret, which is created prior to
    execution.
    Additionally, refactoring end-to-end tests to avoid duplication,
    creating a logical path from cluster setup until execution of test
    cases.
    
    * Container Registry Secret
    
    Only creating secret when both secret-name and payload are informed via
    environment variables.

--- test/e2e/validators.go
+++ test/e2e/validators.go
@@ -2,13 +2,15 @@ package e2e
 
 import (
 	goctx "context"
-	"errors"
 	"io/ioutil"
 	"os"
 	"testing"
 	"time"
 
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
@@ -18,253 +20,256 @@ import (
 
 	buildv1alpha1 "github.com/redhat-developer/build/pkg/apis/build/v1alpha1"
 	taskv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/kubectl/pkg/scheme"
 )
 
-var (
-	EnvVarImageRepo       = "TEST_IMAGE_REPO"
-	EnvVarImageRepoSecret = "TEST_IMAGE_REPO_SECRET"
-	EnvVarSourceURLGithub = "TEST_PRIVATE_GITHUB"
-	EnvVarSourceURLGitlab = "TEST_PRIVATE_GITLAB"
-	EnvVarSourURLSecret   = "TEST_SOURCE_SECRET"
-	QuayHostURL           = "quay.io"
+const (
+	EnvVarImageRepo            = "TEST_IMAGE_REPO"
+	EnvVarEnablePrivateRepos   = "TEST_PRIVATE_REPO"
+	EnvVarImageRepoSecret      = "TEST_IMAGE_REPO_SECRET"
+	EnvVarSourceRepoSecretJSON = "TEST_IMAGE_REPO_DOCKERCONFIGJSON"
+	EnvVarSourceURLGithub      = "TEST_PRIVATE_GITHUB"
+	EnvVarSourceURLGitlab      = "TEST_PRIVATE_GITLAB"
+	EnvVarSourceURLSecret      = "TEST_SOURCE_SECRET"
 )
 
-// OperatorEmulation is used as an struct
-// to hold required data
-type OperatorEmulation struct {
-	buildRun                *operator.BuildRun
-	clusterBuildStrategy    *operator.ClusterBuildStrategy
-	buildStrategy           *operator.BuildStrategy
-	build                   *operator.Build
-	namespace               string
-	identifier              string
-	buildStrategySamplePath string
-	buildSamplePath         string
-	buildRunSamplePath      string
+const TestServiceAccountName = "pipeline"
+
+// cleanupOptions return a CleanupOptions instance.
+func cleanupOptions(ctx *framework.TestCtx) *framework.CleanupOptions {
+	return &framework.CleanupOptions{
+		TestContext:   ctx,
+		Timeout:       cleanupTimeout,
+		RetryInterval: cleanupRetryInterval,
+	}
 }
 
-func newOperatorEmulation(n string, id string, bSPath string, bPath string, bRPath string) *OperatorEmulation {
-	return &OperatorEmulation{
-		buildRun:                &operator.BuildRun{},
-		clusterBuildStrategy:    &operator.ClusterBuildStrategy{},
-		buildStrategy:           &operator.BuildStrategy{},
-		build:                   &operator.Build{},
-		namespace:               n,
-		identifier:              id,
-		buildStrategySamplePath: bSPath,
-		buildSamplePath:         bPath,
-		buildRunSamplePath:      bRPath,
+// createPipelineServiceAccount make sure the "pipeline" SA is created, or already exists.
+func createPipelineServiceAccount(t *testing.T, ctx *framework.TestCtx, f *framework.Framework) {
+	ns, err := ctx.GetNamespace()
+	require.NoError(t, err, "unable to obtain test namespace")
+	serviceAccount := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:      TestServiceAccountName,
+		},
+	}
+	t.Logf("Creating '%s' service-account", TestServiceAccountName)
+	err = f.Client.Create(goctx.TODO(), serviceAccount, cleanupOptions(ctx))
+	if err != nil && !k8serrors.IsAlreadyExists(err) {
+		t.Fatal(err)
+	}
+}
+
+// createContainerRegistrySecret use environment variables to check for container registry
+// credentials secret, when not found a new secret is created.
+func createContainerRegistrySecret(t *testing.T, ctx *framework.TestCtx, f *framework.Framework) {
+	secretName := os.Getenv(EnvVarImageRepoSecret)
+	secretPayload := os.Getenv(EnvVarSourceRepoSecretJSON)
+	if secretName == "" || secretPayload == "" {
+		t.Logf("Container registry secret won't be created.")
+		return
 	}
 
+	ns, err := ctx.GetNamespace()
+	require.NoError(t, err, "unable to obtain test namespace")
+
+	secretNsName := types.NamespacedName{Namespace: ns, Name: secretName}
+	secret := &corev1.Secret{}
+	if err = f.Client.Get(goctx.TODO(), secretNsName, secret); err == nil {
+		t.Logf("Container registry secret is found at '%s/%s'", ns, secretName)
+		return
+	}
+
+	payload := []byte(secretPayload)
+	secret = &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:      secretName,
+		},
+		Type: corev1.SecretTypeDockerConfigJson,
+		Data: map[string][]byte{
+			".dockerconfigjson": payload,
+		},
+	}
+	t.Logf("Creating container-registry secret '%s/%s' (%d bytes)", ns, secretName, len(payload))
+	err = f.Client.Create(goctx.TODO(), secret, cleanupOptions(ctx))
+	require.NoError(t, err, "on creating container registry secret")
 }
 
+// createNamespacedBuildStrategy create a namespaced BuildStrategy.
 func createNamespacedBuildStrategy(
 	t *testing.T,
 	ctx *framework.TestCtx,
 	f *framework.Framework,
-	testBuildStrategy *operator.BuildStrategy) {
-	err := f.Client.Create(goctx.TODO(), testBuildStrategy, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	testBuildStrategy *operator.BuildStrategy,
+) {
+	err := f.Client.Create(goctx.TODO(), testBuildStrategy, cleanupOptions(ctx))
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
+// createClusterBuildStrategy create ClusterBuildStrategy resource.
 func createClusterBuildStrategy(
 	t *testing.T,
 	ctx *framework.TestCtx,
 	f *framework.Framework,
-	testBuildStrategy *operator.ClusterBuildStrategy) {
-	err := f.Client.Create(goctx.TODO(), testBuildStrategy, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
-	if err != nil {
+	testBuildStrategy *operator.ClusterBuildStrategy,
+) {
+	err := f.Client.Create(goctx.TODO(), testBuildStrategy, cleanupOptions(ctx))
+	if err != nil && !k8serrors.IsAlreadyExists(err) {
 		t.Fatal(err)
 	}
 }
 
-func deleteClusterBuildStrategy(
-	t *testing.T,
-	f *framework.Framework,
-	testBuildStrategy *operator.ClusterBuildStrategy) {
-	err := f.Client.Delete(goctx.TODO(), testBuildStrategy)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func deleteBuildStrategy(
-	t *testing.T,
-	f *framework.Framework,
-	testBuildStrategy *operator.BuildStrategy) {
-	err := f.Client.Delete(goctx.TODO(), testBuildStrategy)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
+// validateController create and watch the build flow happening, probing each step for a image
+// successfully created.
 func validateController(
 	t *testing.T,
 	ctx *framework.TestCtx,
 	f *framework.Framework,
 	testBuild *operator.Build,
-	testBuildRun *operator.BuildRun) {
-	namespace, _ := ctx.GetNamespace()
+	testBuildRun *operator.BuildRun,
+) {
+	ns, _ := ctx.GetNamespace()
 	pendingStatus := "Pending"
 	runningStatus := "Running"
 	trueCondition := v1.ConditionTrue
+	pendingAndRunningStatues := []string{pendingStatus, runningStatus}
 
 	// Ensure the Build has been created
-	err := f.Client.Create(goctx.TODO(), testBuild, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	err := f.Client.Create(goctx.TODO(), testBuild, cleanupOptions(ctx))
 	require.NoError(t, err)
 
 	// Ensure the BuildRun has been created
-	err = f.Client.Create(goctx.TODO(), testBuildRun, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	err = f.Client.Create(goctx.TODO(), testBuildRun, cleanupOptions(ctx))
 	require.NoError(t, err)
-
-	time.Sleep(15 * time.Second)
 
 	// Ensure that a TaskRun has been created and is in pending or running state
-	generatedTaskRun, err := getTaskRun(testBuild, testBuildRun, f)
-	require.NoError(t, err)
-	require.Contains(t, [2]string{pendingStatus, runningStatus}, generatedTaskRun.Status.Conditions[0].Reason, "TaskRun not pending or running")
+	require.Eventually(t, func() bool {
+		taskRun, err := getTaskRun(f, testBuild, testBuildRun)
+		if err != nil {
+			t.Logf("Retrieveing TaskRun error: '%s'", err)
+			return false
+		}
+		if taskRun == nil {
+			t.Log("TaskRun is not yet generated!")
+			return false
+		}
+		if len(taskRun.Status.Conditions) == 0 {
+			return false
+		}
+		conditionReason := taskRun.Status.Conditions[0].Reason
+		t.Logf("TaskRun condition reason: '%s'", conditionReason)
+		return conditionReason == pendingStatus || conditionReason == runningStatus
+	}, 300*time.Second, 5*time.Second, "TaskRun is not pending or running")
 
 	// Ensure BuildRun is in pending or running state
-	err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: testBuildRun.Name, Namespace: namespace}, testBuildRun)
+	buildRunNsName := types.NamespacedName{Name: testBuildRun.Name, Namespace: ns}
+	err = f.Client.Get(goctx.TODO(), buildRunNsName, testBuildRun)
 	require.NoError(t, err)
-	require.Contains(t, [2]string{pendingStatus, runningStatus}, testBuildRun.Status.Reason, "BuildRun not pending or running")
+	reason := testBuildRun.Status.Reason
+	require.Contains(t, pendingAndRunningStatues, reason, "BuildRun not pending or running")
 
 	// Ensure that Build moves to Running State
 	require.Eventually(t, func() bool {
-		err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: testBuildRun.Name, Namespace: namespace}, testBuildRun)
+		err = f.Client.Get(goctx.TODO(), buildRunNsName, testBuildRun)
 		require.NoError(t, err)
 
 		return testBuildRun.Status.Reason == runningStatus
-	}, 30*time.Second, 3*time.Second, "BuildRun not running")
+	}, 180*time.Second, 3*time.Second, "BuildRun not running")
 
 	// Ensure that eventually the Build moves to Succeeded.
 	require.Eventually(t, func() bool {
-		err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: testBuildRun.Name, Namespace: namespace}, testBuildRun)
+		err = f.Client.Get(goctx.TODO(), buildRunNsName, testBuildRun)
 		require.NoError(t, err)
 
 		return testBuildRun.Status.Succeeded == trueCondition
-	}, 300*time.Second, 10*time.Second, "BuildRun not succeeded")
+	}, 550*time.Second, 5*time.Second, "BuildRun not succeeded")
+
+	t.Logf("Test build complete '%s'!", testBuildRun.GetName())
 }
 
-// BuildTestData loads all different Build objects
-// into the OperatorEmulation structure
-func BuildTestData(oE *OperatorEmulation) error {
-
-	// Load the ClusterBuildStrategy sample into the OperatorEmulation.clusterBuildS field
-	if err := oE.LoadBuildSamples(oE.buildStrategySamplePath); err != nil {
-		return err
-	}
-
-	// Load the Build sample into the OperatorEmulation.build field
-	if err := oE.LoadBuildSamples(oE.buildSamplePath); err != nil {
-		return err
-	}
-
-	// Load the BuildRun sample into the OperatorEmulation.buildRun field
-	if err := oE.LoadBuildSamples(oE.buildRunSamplePath); err != nil {
-		return err
-	}
-	return nil
-}
-
-// LoadBuildSamples populates Build objects depending on the
-// object type
-func (os *OperatorEmulation) LoadBuildSamples(buildStrategySample string) error {
-
+// readAndDecode read file path and decode.
+func readAndDecode(filePath string) (runtime.Object, error) {
 	decode := scheme.Codecs.UniversalDeserializer().Decode
-	operatorapis.AddToScheme(scheme.Scheme)
-
-	y, err := ioutil.ReadFile(buildStrategySample)
+	err := operatorapis.AddToScheme(scheme.Scheme)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	obj, _, err := decode([]byte(y), nil, nil)
+	payload, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	switch object := obj.(type) {
-	case *operator.Build:
-		os.build = object
-		os.build.SetNamespace(os.namespace)
-		os.build.SetName(os.identifier)
-		return nil
-	case *operator.BuildRun:
-		os.buildRun = object
-		os.buildRun.SetNamespace(os.namespace)
-		os.buildRun.SetName(os.identifier)
-		os.buildRun.Spec.BuildRef.Name = os.identifier
-		return nil
-	case *operator.ClusterBuildStrategy:
-		os.clusterBuildStrategy = object
-		return nil
-	case *operator.BuildStrategy:
-		os.buildStrategy = object
-		return nil
-	default:
-		return errors.New("none build strategy identified")
-	}
+	obj, _, err := decode([]byte(payload), nil, nil)
+	return obj, err
 }
 
-// validateOutputEnvVars looks for known environment variables
-// in order to modify on the fly specific Build object specs:
-// - Spec.Output.ImageURL
-// - Spec.Output.SecretRef
-func validateOutputEnvVars(o *operator.Build) {
-
-	// Get TEST_IMAGE_REPO env variable
-	if val, bool := os.LookupEnv(EnvVarImageRepo); bool {
-		o.Spec.Output.ImageURL = val
+// buildStrategyTestData gets the us the BuildStrategy test data set up
+func buildStrategyTestData(ns string, buildStrategyCRPath string) (*operator.BuildStrategy, error) {
+	obj, err := readAndDecode(buildStrategyCRPath)
+	if err != nil {
+		return nil, err
 	}
 
-	// Get TEST_IMAGE_REPO_SECRET env variable
-	if s, bool := os.LookupEnv(EnvVarImageRepoSecret); bool {
-		o.Spec.Output.SecretRef = &v1.LocalObjectReference{
-			Name: s,
-		}
-	}
+	buildStrategy := obj.(*operator.BuildStrategy)
+	buildStrategy.SetNamespace(ns)
+
+	return buildStrategy, err
 }
 
-// validateRegistryEnvVars check if the TEST_IMAGE_REPO
-// and TEST_IMAGE_REPO_SECRET env variables are set
-func validateRegistryEnvVars() bool {
-	if os.Getenv(EnvVarImageRepo) != "" && os.Getenv(EnvVarImageRepoSecret) != "" {
-		return true
+// clusterBuildStrategyTestData gets the us the ClusterBuildStrategy test data set up
+func clusterBuildStrategyTestData(buildStrategyCRPath string) (*operator.ClusterBuildStrategy, error) {
+	obj, err := readAndDecode(buildStrategyCRPath)
+	if err != nil {
+		return nil, err
 	}
-	return false
+
+	clusterBuildStrategy := obj.(*operator.ClusterBuildStrategy)
+	return clusterBuildStrategy, err
 }
 
-func validateGithubURL(o *operator.Build) {
-	if val, bool := os.LookupEnv(EnvVarSourceURLGithub); bool {
-		o.Spec.Source.URL = val
+// buildTestData gets the us the Build test data set up
+func buildTestData(ns string, identifier string, buildCRPath string) (*operator.Build, error) {
+	obj, err := readAndDecode(buildCRPath)
+	if err != nil {
+		return nil, err
 	}
+
+	build := obj.(*operator.Build)
+	build.SetNamespace(ns)
+	build.SetName(identifier)
+	return build, err
 }
 
-func validateGitlabURL(o *operator.Build) {
-	if val, bool := os.LookupEnv(EnvVarSourceURLGitlab); bool {
-		o.Spec.Source.URL = val
+// buildTestData gets the us the Build test data set up
+func buildRunTestData(ns string, identifier string, buildRunCRPath string) (*operator.BuildRun, error) {
+	obj, err := readAndDecode(buildRunCRPath)
+	if err != nil {
+		return nil, err
 	}
+
+	buildRun := obj.(*operator.BuildRun)
+	buildRun.SetNamespace(ns)
+	buildRun.SetName(identifier)
+	buildRun.Spec.BuildRef.Name = identifier
+	return buildRun, err
 }
 
-func validateSourceSecretRef(o *operator.Build) {
-	// Get TEST_SOURCE_SECRET env variable
-	if s, bool := os.LookupEnv(EnvVarSourURLSecret); bool {
-		o.Spec.Source.SecretRef = &v1.LocalObjectReference{
-			Name: s,
-		}
-	}
-}
-
-func getTaskRun(build *buildv1alpha1.Build, buildRun *buildv1alpha1.BuildRun, f *framework.Framework) (*taskv1.TaskRun, error) {
-
+// getTaskRun retrieve Tekton's Task based on BuildRun instance.
+func getTaskRun(
+	f *framework.Framework,
+	build *buildv1alpha1.Build,
+	buildRun *buildv1alpha1.BuildRun,
+) (*taskv1.TaskRun, error) {
 	taskRunList := &taskv1.TaskRunList{}
-
 	lbls := map[string]string{
 		buildv1alpha1.LabelBuild:    build.Name,
 		buildv1alpha1.LabelBuildRun: buildRun.Name,
@@ -274,11 +279,9 @@ func getTaskRun(build *buildv1alpha1.Build, buildRun *buildv1alpha1.BuildRun, f 
 		LabelSelector: labels.SelectorFromSet(lbls),
 	}
 	err := f.Client.List(goctx.TODO(), taskRunList, &opts)
-
 	if err != nil {
 		return nil, err
 	}
-
 	if len(taskRunList.Items) > 0 {
 		return &taskRunList.Items[len(taskRunList.Items)-1], nil
 	}
