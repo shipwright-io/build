22e8e7179060958d9cf54fe5d67cc73356a34fe0
Otávio Fernandes <otaviof@gmail.com>
2020-04-28 19:15:09 +0200

    Splitting up scripts, adding container registry.

---
+++ hack/install-kubectl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Installs "kubectl" on Travis-CI Ubuntu.
+#
+
+set -eu
+
+sudo apt-get update > /dev/null && \
+    sudo apt-get install -y \
+        apt-transport-https \
+        curl \
+        git
+
+#
+# Kubectl
+#
+
+echo "# Configuring kubectl APT repository..."
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+    |sudo apt-key add -
+
+if [ ! -f "/etc/apt/sources.list.d/kubernetes.list" ] ; then
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" \
+        |sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+fi
+
+echo "# Installing kubectl..."
+sudo apt-get update && \
+    sudo apt-get install -y kubectl
+
+echo "# Kubectl version: `kubectl version`"
