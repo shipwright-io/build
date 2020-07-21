e44a728d9d4d7d8284d1be7ff507f735fdbb2eb8
Sascha Schwarze <schwarzs@de.ibm.com>
2020-07-21 12:10:39 +0200

    Refine build_buildrun_establish_duration_seconds buckets and make buckets configurable (#298)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,19 +3,32 @@ package config
 import (
 	"os"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 const (
 	contextTimeout = 300 * time.Second
 	// A number in seconds to define a context Timeout
 	// E.g. if 5 seconds is wanted, the CTX_TIMEOUT=5
-	contexTimeoutEnvVar = "CTX_TIMEOUT"
+	contextTimeoutEnvVar = "CTX_TIMEOUT"
 
 	kanikoDefaultImage = "gcr.io/kaniko-project/executor:v0.24.0"
 	// kanikoImageEnvVar environment variable for Kaniko container image, for instance:
 	// KANIKO_CONTAINER_IMAGE="gcr.io/kaniko-project/executor:v0.24.0"
 	kanikoImageEnvVar = "KANIKO_CONTAINER_IMAGE"
+
+	// environment variable to override the buckets
+	metricBuildRunCompletionDurationBucketsEnvVar = "PROMETHEUS_BR_COMP_DUR_BUCKETS"
+	metricBuildRunEstablishDurationBucketsEnvVar  = "PROMETHEUS_BR_EST_DUR_BUCKETS"
+)
+
+var (
+	// arrays are not possible as constants
+	metricBuildRunCompletionDurationBuckets = prometheus.LinearBuckets(50, 50, 10)
+	metricBuildRunEstablishDurationBuckets  = []float64{0, 1, 2, 3, 5, 7, 10, 15, 20, 30}
 )
 
 // Config hosts different parameters that
@@ -23,6 +36,13 @@ const (
 type Config struct {
 	CtxTimeOut           time.Duration
 	KanikoContainerImage string
+	Prometheus           PrometheusConfig
+}
+
+// PrometheusConfig contains the specific configuration for the
+type PrometheusConfig struct {
+	BuildRunCompletionDurationBuckets []float64
+	BuildRunEstablishDurationBuckets  []float64
 }
 
 // NewDefaultConfig returns a new Config, with context timeout and default Kaniko image.
@@ -30,12 +50,16 @@ func NewDefaultConfig() *Config {
 	return &Config{
 		CtxTimeOut:           contextTimeout,
 		KanikoContainerImage: kanikoDefaultImage,
+		Prometheus: PrometheusConfig{
+			BuildRunCompletionDurationBuckets: metricBuildRunCompletionDurationBuckets,
+			BuildRunEstablishDurationBuckets:  metricBuildRunEstablishDurationBuckets,
+		},
 	}
 }
 
 // SetConfigFromEnv updates the configuration managed by environment variables.
 func (c *Config) SetConfigFromEnv() error {
-	timeout := os.Getenv(contexTimeoutEnvVar)
+	timeout := os.Getenv(contextTimeoutEnvVar)
 	if timeout != "" {
 		i, err := strconv.Atoi(timeout)
 		if err != nil {
@@ -48,5 +72,38 @@ func (c *Config) SetConfigFromEnv() error {
 	if kanikoImage != "" {
 		c.KanikoContainerImage = kanikoImage
 	}
+
+	buildRunCompletionDurationBucketsEnvVarValue := os.Getenv(metricBuildRunCompletionDurationBucketsEnvVar)
+	if buildRunCompletionDurationBucketsEnvVarValue != "" {
+		buildRunCompletionDurationBuckets, err := stringToFloat64Array(strings.Split(buildRunCompletionDurationBucketsEnvVarValue, ","))
+		if err != nil {
+			return err
+		}
+		c.Prometheus.BuildRunCompletionDurationBuckets = buildRunCompletionDurationBuckets
+	}
+
+	buildRunEstablishDurationBucketsEnvVarValue := os.Getenv(metricBuildRunEstablishDurationBucketsEnvVar)
+	if buildRunEstablishDurationBucketsEnvVarValue != "" {
+		buildRunEstablishDurationBuckets, err := stringToFloat64Array(strings.Split(buildRunEstablishDurationBucketsEnvVarValue, ","))
+		if err != nil {
+			return err
+		}
+		c.Prometheus.BuildRunEstablishDurationBuckets = buildRunEstablishDurationBuckets
+	}
+
 	return nil
+}
+
+func stringToFloat64Array(strings []string) ([]float64, error) {
+	floats := make([]float64, len(strings))
+
+	for i, string := range strings {
+		float, err := strconv.ParseFloat(string, 64)
+		if err != nil {
+			return nil, err
+		}
+		floats[i] = float
+	}
+
+	return floats, nil
 }
