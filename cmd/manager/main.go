af84cbb08d60c5595dbc9b3fc3847ab3820714c0
Xiu Juan Xiang <xigxjn@cn.ibm.com>
2020-08-12 18:28:53 +0800

    remove leader-for-life HA mode

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
-	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
 	"github.com/operator-framework/operator-sdk/pkg/ready"
@@ -98,13 +97,6 @@ func main() {
 		os.Exit(1)
 	}
 	defer r.Unset()
-
-	// Become the leader before proceeding
-	err = leader.Become(ctx, "build-operator-lock")
-	if err != nil {
-		ctxlog.Error(ctx, err, "")
-		os.Exit(1)
-	}
 
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
