b47230fd25a5b345cc65cc04bd6979080515e57b
Adam Kaplan <adam.kaplan@redhat.com>
2020-11-02 17:31:23 -0500

    bump(*):
    - Make go 1.14 explicit
    - Bump operator-framework/operator-sdk to v0.18.2
    - Bump k8s.io/* to v0.18.10
    - Replace for github.com/Sirupsen/logrus (renamed)
    - Replace for github.com/docker/distribution (fix version tag resolution)
    - Pin docker/docker to specific version
    - Update knative.dev/pkg to release-0.18
    - Update tekton to d5df1c164a48 to get k8s 1.18 clientsets
    - go mod tidy && go mod vendor

--- vendor/github.com/operator-framework/operator-sdk/internal/scaffold/go_mod.go
+++ vendor/github.com/operator-framework/operator-sdk/internal/scaffold/go_mod.go
@@ -41,12 +41,12 @@ const goModTmpl = `module {{ .Repo }}
 go 1.13
 
 require (
-	github.com/operator-framework/operator-sdk v0.17.0
-	sigs.k8s.io/controller-runtime v0.5.2
+	github.com/operator-framework/operator-sdk v0.18.2
+	sigs.k8s.io/controller-runtime v0.6.0
 )
 
 replace (
-	k8s.io/client-go => k8s.io/client-go v0.17.4 // Required by prometheus-operator
+	k8s.io/client-go => k8s.io/client-go v0.18.2 // Required by prometheus-operator
 	github.com/Azure/go-autorest => github.com/Azure/go-autorest v13.3.2+incompatible // Required by OLM
 )
 `
