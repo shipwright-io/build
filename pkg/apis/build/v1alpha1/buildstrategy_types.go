7f4143eb88824882a5d9568ebe5312b4b41a2750
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-03 17:34:24 +0100

    Move common build strategy code into separate file
    The `buildstrategy_types.go` contains code that is common and should be externalised.
    
    Move common code that applies to both kinds of strategies into a separate file.

--- pkg/apis/build/v1alpha1/buildstrategy_types.go
+++ pkg/apis/build/v1alpha1/buildstrategy_types.go
@@ -5,24 +5,18 @@
 package v1alpha1
 
 import (
-	corev1 "k8s.io/api/core/v1"
+	"strconv"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// BuildStrategySpec defines the desired state of BuildStrategy
-type BuildStrategySpec struct {
-	BuildSteps []BuildStep `json:"buildSteps,omitempty"`
-}
+const (
+	// LabelBuildStrategyName is a label key for defining the build strategy name
+	LabelBuildStrategyName = "buildstrategy.build.dev/name"
 
-// BuildStep defines a partial step that needs to run in container for
-// building the image.
-type BuildStep struct {
-	corev1.Container `json:",inline"`
-}
-
-// BuildStrategyStatus defines the observed state of BuildStrategy
-type BuildStrategyStatus struct {
-}
+	// LabelBuildStrategyGeneration is a label key for defining the build strategy generation
+	LabelBuildStrategyGeneration = "buildstrategy.build.dev/generation"
+)
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -50,25 +44,3 @@ type BuildStrategyList struct {
 func init() {
 	SchemeBuilder.Register(&BuildStrategy{}, &BuildStrategyList{})
 }
-
-// StrategyRef can be used to refer to a specific instance of a buildstrategy.
-// Copied from CrossVersionObjectReference: https://github.com/kubernetes/kubernetes/blob/169df7434155cbbc22f1532cba8e0a9588e29ad8/pkg/apis/autoscaling/types.go#L64
-type StrategyRef struct {
-	// Name of the referent; More info: http://kubernetes.io/docs/user-guide/identifiers#names
-	Name string `json:"name"`
-	// BuildStrategyKind indicates the kind of the buildstrategy, namespaced or cluster scoped.
-	Kind *BuildStrategyKind `json:"kind,omitempty"`
-	// API version of the referent
-	// +optional
-	APIVersion string `json:"apiVersion,omitempty"`
-}
-
-// BuildStrategyKind defines the type of BuildStrategy used by the build.
-type BuildStrategyKind string
-
-const (
-	// NamespacedBuildStrategyKind indicates that the buildstrategy type has a namespaced scope.
-	NamespacedBuildStrategyKind BuildStrategyKind = "BuildStrategy"
-	// ClusterBuildStrategyKind indicates that buildstrategy type has a cluster scope.
-	ClusterBuildStrategyKind BuildStrategyKind = "ClusterBuildStrategy"
-)
