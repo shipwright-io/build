6263f4013b97dbc3491151d6eba5cafb97eaf77e
Matthias Diester <matthias.diester@de.ibm.com>
2021-09-16 13:36:03 +0200

    Use `go install` instead of `go get`
    With Go 1.16, switch to `go install` for tools in preparation of the `go get`r
    install deprecation.
    
    Change `counterfeiter` location from needing to be under `GOPATH` to ber
    anywhere as long as it is in the `PATH`.
    
    Signed-off-by: Mayukh Sarkar <mayukh.sarkar1@ibm.com>

--- hack/generate-fakes.sh
+++ hack/generate-fakes.sh
@@ -10,8 +10,7 @@ set -euo pipefail
 [ ! -d "vendor" ] && echo "$0 requires vendor/ folder, run 'go mod vendor'"
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-BIN=$(go env GOPATH)/bin
 
-GO111MODULE=off "${BIN}/counterfeiter" -header "${SCRIPT_ROOT}/hack/boilerplate.go.txt" -o pkg/controller/fakes/manager.go vendor/sigs.k8s.io/controller-runtime/pkg/manager Manager
-GO111MODULE=off "${BIN}/counterfeiter" -header "${SCRIPT_ROOT}/hack/boilerplate.go.txt" -o pkg/controller/fakes/client.go vendor/sigs.k8s.io/controller-runtime/pkg/client Client
-GO111MODULE=off "${BIN}/counterfeiter" -header "${SCRIPT_ROOT}/hack/boilerplate.go.txt" -o pkg/controller/fakes/status_writer.go vendor/sigs.k8s.io/controller-runtime/pkg/client StatusWriter
+GO111MODULE=off counterfeiter -header "${SCRIPT_ROOT}/hack/boilerplate.go.txt" -o pkg/controller/fakes/manager.go vendor/sigs.k8s.io/controller-runtime/pkg/manager Manager
+GO111MODULE=off counterfeiter -header "${SCRIPT_ROOT}/hack/boilerplate.go.txt" -o pkg/controller/fakes/client.go vendor/sigs.k8s.io/controller-runtime/pkg/client Client
+GO111MODULE=off counterfeiter -header "${SCRIPT_ROOT}/hack/boilerplate.go.txt" -o pkg/controller/fakes/status_writer.go vendor/sigs.k8s.io/controller-runtime/pkg/client StatusWriter
