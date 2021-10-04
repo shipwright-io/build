bfdeb639b8216ad98e38285ebad8a7957ae7b99f
Sascha Schwarze <schwarzs@de.ibm.com>
2021-10-04 15:55:37 +0200

    Remove -sa suffix from generated service account

--- test/e2e/validators_test.go
+++ test/e2e/validators_test.go
@@ -249,7 +249,7 @@ func validateServiceAccountDeletion(buildRun *buildv1alpha1.BuildRun, namespace 
 	}
 
 	saNamespacedName := types.NamespacedName{
-		Name:      buildRun.Name + "-sa",
+		Name:      buildRun.Name,
 		Namespace: namespace,
 	}
 
