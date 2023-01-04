bfce81e95ceea685048d0bb8b05580baf0bb0fa9
Matthias Diester <matthias.diester@de.ibm.com>
2023-01-04 22:23:44 +0100

    Redact incline credentials in log
    Introduce display URL that is a safe version of the provided URL to be
    used for logging.
    
    Refactor test case code in order to allow for more flexibility.

--- cmd/git/main_test.go
+++ cmd/git/main_test.go
@@ -5,13 +5,16 @@
 package main_test
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -20,20 +23,45 @@ import (
 	shpgit "github.com/shipwright-io/build/pkg/git"
 )
 
-var _ = Describe("Git Resource", func() {
-	var run = func(args ...string) error {
-		// discard log output
-		log.SetOutput(io.Discard)
+type opts struct {
+	ctx       context.Context
+	logOutput io.Writer
+	args      []string
+}
 
-		// discard stderr output
-		var tmp = os.Stderr
-		os.Stderr = nil
-		defer func() { os.Stderr = tmp }()
+type runOpts func(*opts)
 
-		os.Args = append([]string{"tool", "--skip-validation"}, args...)
-		return Execute(context.TODO())
+func withLogOutput(out io.Writer) runOpts { return func(o *opts) { o.logOutput = out } }
+func withArgs(args ...string) runOpts     { return func(o *opts) { o.args = args } }
+
+func run(o ...runOpts) error {
+	var settings = &opts{}
+	for _, entry := range o {
+		entry(settings)
 	}
 
+	// context default: use context.TODO()
+	if settings.ctx == nil {
+		settings.ctx = context.TODO()
+	}
+
+	// log output default: discard
+	if settings.logOutput == nil {
+		settings.logOutput = io.Discard
+	}
+
+	log.SetOutput(settings.logOutput)
+
+	// discard stderr output
+	var tmp = os.Stderr
+	os.Stderr = nil
+	defer func() { os.Stderr = tmp }()
+
+	os.Args = append([]string{"tool", "--skip-validation"}, settings.args...)
+	return Execute(settings.ctx)
+}
+
+var _ = Describe("Git Resource", func() {
 	var withTempDir = func(f func(target string)) {
 		path, err := os.MkdirTemp(os.TempDir(), "git")
 		Expect(err).ToNot(HaveOccurred())
@@ -62,7 +90,7 @@ var _ = Describe("Git Resource", func() {
 
 	Context("validations and error cases", func() {
 		It("should succeed in case the help is requested", func() {
-			Expect(run("--help")).ToNot(HaveOccurred())
+			Expect(run(withArgs("--help"))).ToNot(HaveOccurred())
 		})
 
 		It("should fail in case mandatory arguments are missing", func() {
@@ -70,36 +98,36 @@ var _ = Describe("Git Resource", func() {
 		})
 
 		It("should fail in case --url is empty", func() {
-			Expect(run(
+			Expect(run(withArgs(
 				"--url", "",
 				"--target", "/workspace/source",
-			)).To(HaveOccurred())
+			))).To(HaveOccurred())
 		})
 
 		It("should fail in case --target is empty", func() {
-			Expect(run(
+			Expect(run(withArgs(
 				"--url", "https://github.com/foo/bar",
 				"--target", "",
-			)).To(HaveOccurred())
+			))).To(HaveOccurred())
 		})
 
 		It("should fail in case url does not exist", func() {
 			withTempDir(func(target string) {
-				Expect(run(
+				Expect(run(withArgs(
 					"--url", "http://github.com/feqlQoDIHc/bcfHFHHXYF",
 					"--target", target,
-				)).To(HaveOccurred())
+				))).To(HaveOccurred())
 			})
 		})
 
 		It("should fail in case secret path content is not recognized", func() {
 			withTempDir(func(secret string) {
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", "https://github.com/foo/bar",
 						"--target", target,
 						"--secret-path", secret,
-					)).To(HaveOccurred())
+					))).To(HaveOccurred())
 				})
 			})
 		})
@@ -110,10 +138,10 @@ var _ = Describe("Git Resource", func() {
 
 		It("should Git clone a repository to the specified target directory", func() {
 			withTempDir(func(target string) {
-				Expect(run(
+				Expect(run(withArgs(
 					"--url", exampleRepo,
 					"--target", target,
-				)).ToNot(HaveOccurred())
+				))).ToNot(HaveOccurred())
 
 				Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 			})
@@ -121,11 +149,11 @@ var _ = Describe("Git Resource", func() {
 
 		It("should Git clone a repository to the specified target directory using a specified branch", func() {
 			withTempDir(func(target string) {
-				Expect(run(
+				Expect(run(withArgs(
 					"--url", exampleRepo,
 					"--target", target,
 					"--revision", "main",
-				)).ToNot(HaveOccurred())
+				))).ToNot(HaveOccurred())
 
 				Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 			})
@@ -134,12 +162,12 @@ var _ = Describe("Git Resource", func() {
 		It("should Git clone a repository to the specified target directory using a specified tag", func() {
 			withTempFile("commit-sha", func(filename string) {
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--target", target,
 						"--revision", "v0.1.0",
 						"--result-file-commit-sha", filename,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filecontent(filename)).To(Equal("8016b0437a7a09079f961e5003e81e5ad54e6c26"))
 				})
@@ -149,12 +177,12 @@ var _ = Describe("Git Resource", func() {
 		It("should Git clone a repository to the specified target directory using a specified commit-sha (long)", func() {
 			withTempFile("commit-sha", func(filename string) {
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--target", target,
 						"--revision", "0e0583421a5e4bf562ffe33f3651e16ba0c78591",
 						"--result-file-commit-sha", filename,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filecontent(filename)).To(Equal("0e0583421a5e4bf562ffe33f3651e16ba0c78591"))
 				})
@@ -164,12 +192,12 @@ var _ = Describe("Git Resource", func() {
 		It("should Git clone a repository to the specified target directory using a specified commit-sha (short)", func() {
 			withTempFile("commit-sha", func(filename string) {
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--target", target,
 						"--revision", "0e05834",
 						"--result-file-commit-sha", filename,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filecontent(filename)).To(Equal("0e0583421a5e4bf562ffe33f3651e16ba0c78591"))
 				})
@@ -194,22 +222,22 @@ var _ = Describe("Git Resource", func() {
 				file(filepath.Join(secret, "ssh-privatekey"), 0400, []byte(sshPrivateKey))
 
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", "https://github.com/foo/bar",
 						"--secret-path", secret,
 						"--target", target,
-					)).To(HaveOccurred())
+					))).To(HaveOccurred())
 				})
 			})
 		})
 
 		It("should fail in case a SSH Git URL is provided but no private key", func() {
 			withTempDir(func(target string) {
-				Expect(run(
+				Expect(run(withArgs(
 					"--url", exampleRepo,
 					"--secret-path", "/tmp/foobar",
 					"--target", target,
-				)).To(HaveOccurred())
+				))).To(HaveOccurred())
 			})
 		})
 
@@ -225,11 +253,11 @@ var _ = Describe("Git Resource", func() {
 				file(filepath.Join(secret, "known_hosts"), 0600, []byte(knownHosts))
 
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--secret-path", secret,
 						"--target", target,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 				})
@@ -242,11 +270,11 @@ var _ = Describe("Git Resource", func() {
 				file(filepath.Join(secret, "ssh-privatekey"), 0400, []byte(sshPrivateKey))
 
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--secret-path", secret,
 						"--target", target,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 				})
@@ -259,12 +287,12 @@ var _ = Describe("Git Resource", func() {
 				file(filepath.Join(secret, "ssh-privatekey"), 0400, []byte(sshPrivateKey))
 
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", "https://github.com/shipwright-io/sample-nodejs-private.git",
 						"--secret-path", secret,
 						"--target", target,
 						"--git-url-rewrite",
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 				})
@@ -277,12 +305,12 @@ var _ = Describe("Git Resource", func() {
 				file(filepath.Join(secret, "ssh-privatekey"), 0400, []byte(sshPrivateKey))
 
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", "git@github.com:shipwright-io/sample-submodule-private.git",
 						"--secret-path", secret,
 						"--target", target,
 						"--git-url-rewrite",
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 					Expect(filepath.Join(target, "src", "sample-nodejs-private", "README.md")).To(BeAnExistingFile())
@@ -315,11 +343,11 @@ var _ = Describe("Git Resource", func() {
 					file(filepath.Join(secret, "password"), 0400, []byte(password))
 
 					withTempDir(func(target string) {
-						Expect(run(
+						Expect(run(withArgs(
 							"--url", exampleRepo,
 							"--secret-path", secret,
 							"--target", target,
-						)).To(HaveOccurred())
+						))).To(HaveOccurred())
 					})
 				})
 			})
@@ -333,11 +361,11 @@ var _ = Describe("Git Resource", func() {
 					file(filepath.Join(secret, "password"), 0400, []byte(password))
 
 					withTempDir(func(target string) {
-						Expect(run(
+						Expect(run(withArgs(
 							"--url", exampleRepo,
 							"--secret-path", secret,
 							"--target", target,
-						)).ToNot(HaveOccurred())
+						))).ToNot(HaveOccurred())
 
 						Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 					})
@@ -353,12 +381,29 @@ var _ = Describe("Git Resource", func() {
 					file(filepath.Join(secret, "password"), 0400, []byte(password))
 
 					withTempDir(func(target string) {
-						Expect(run(
+						Expect(run(withArgs(
 							"--url", "http://github.com/shipwright-io/sample-nodejs-private",
 							"--secret-path", secret,
 							"--target", target,
-						)).To(FailWith(shpgit.AuthUnexpectedHTTP))
+						))).To(FailWith(shpgit.AuthUnexpectedHTTP))
 					})
+				})
+			})
+		})
+
+		It("should detect inline credentials and make sure to redact these in the logs", func() {
+			withUsernamePassword(func(username, password string) {
+				withTempDir(func(target string) {
+					var buf bytes.Buffer
+					Expect(run(
+						withLogOutput(&buf),
+						withArgs(
+							"--url", fmt.Sprintf("https://%s:%s@github.com/shipwright-io/sample-nodejs-private", username, password),
+							"--target", target,
+						),
+					)).To(Succeed())
+
+					Expect(strings.Count(buf.String(), password)).To(BeZero())
 				})
 			})
 		})
@@ -369,10 +414,10 @@ var _ = Describe("Git Resource", func() {
 
 		It("should Git clone a repository with a submodule", func() {
 			withTempDir(func(target string) {
-				Expect(run(
+				Expect(run(withArgs(
 					"--url", exampleRepo,
 					"--target", target,
-				)).ToNot(HaveOccurred())
+				))).ToNot(HaveOccurred())
 
 				Expect(filepath.Join(target, "README.md")).To(BeAnExistingFile())
 				Expect(filepath.Join(target, "themes", "docsy", "README.md")).To(BeAnExistingFile())
@@ -386,12 +431,12 @@ var _ = Describe("Git Resource", func() {
 		It("should store commit-sha into file specified in --result-file-commit-sha flag", func() {
 			withTempFile("commit-sha", func(filename string) {
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--target", target,
 						"--revision", "v0.1.0",
 						"--result-file-commit-sha", filename,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filecontent(filename)).To(Equal("8016b0437a7a09079f961e5003e81e5ad54e6c26"))
 				})
@@ -400,14 +445,13 @@ var _ = Describe("Git Resource", func() {
 
 		It("should store commit-author into file specified in --result-file-commit-author flag", func() {
 			withTempFile("commit-author", func(filename string) {
-
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--target", target,
 						"--revision", "v0.1.0",
 						"--result-file-commit-author", filename,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filecontent(filename)).To(Equal("Enrique Encalada"))
 				})
@@ -416,13 +460,12 @@ var _ = Describe("Git Resource", func() {
 
 		It("should store branch-name into file specified in --result-file-branch-name flag", func() {
 			withTempFile("branch-name", func(filename string) {
-
 				withTempDir(func(target string) {
-					Expect(run(
+					Expect(run(withArgs(
 						"--url", exampleRepo,
 						"--target", target,
 						"--result-file-branch-name", filename,
-					)).ToNot(HaveOccurred())
+					))).ToNot(HaveOccurred())
 
 					Expect(filecontent(filename)).To(Equal("main"))
 				})
@@ -431,9 +474,7 @@ var _ = Describe("Git Resource", func() {
 	})
 
 	Context("Some tests mutate or depend on git configurations. They must run sequentially to avoid race-conditions.", Ordered, func() {
-
 		Context("Test that require git configurations", func() {
-
 			Context("cloning repositories with Git Large File Storage", func() {
 				const exampleRepo = "https://github.com/shipwright-io/sample-lfs"
 
@@ -445,10 +486,10 @@ var _ = Describe("Git Resource", func() {
 
 				It("should Git clone a repository to the specified target directory", func() {
 					withTempDir(func(target string) {
-						Expect(run(
+						Expect(run(withArgs(
 							"--url", exampleRepo,
 							"--target", target,
-						)).ToNot(HaveOccurred())
+						))).ToNot(HaveOccurred())
 
 						lfsFile := filepath.Join(target, "assets", "shipwright-logo-lightbg-512.png")
 						Expect(lfsFile).To(BeAnExistingFile())
@@ -498,11 +539,11 @@ var _ = Describe("Git Resource", func() {
 							file(filepath.Join(secret, "password"), 0400, []byte("ghp_sFhFsSHhTzMDreGRLjmks4Tzuzgthdvfsrta"))
 
 							withTempDir(func(target string) {
-								err := run(
+								err := run(withArgs(
 									"--url", repo,
 									"--secret-path", secret,
 									"--target", target,
-								)
+								))
 
 								Expect(err).ToNot(BeNil())
 
@@ -522,11 +563,11 @@ var _ = Describe("Git Resource", func() {
 						withTempDir(func(target string) {
 							withTempDir(func(secret string) {
 								file(filepath.Join(secret, "ssh-privatekey"), 0400, []byte("invalid"))
-								err := run(
+								err := run(withArgs(
 									"--url", repo,
 									"--target", target,
 									"--secret-path", secret,
-								)
+								))
 
 								Expect(err).ToNot(BeNil())
 
@@ -543,10 +584,10 @@ var _ = Describe("Git Resource", func() {
 				It("should prompt auth for non-existing or private repo", func() {
 					testForRepo := func(repo string) {
 						withTempDir(func(target string) {
-							err := run(
+							err := run(withArgs(
 								"--url", repo,
 								"--target", target,
-							)
+							))
 
 							Expect(err).ToNot(BeNil())
 
@@ -563,11 +604,11 @@ var _ = Describe("Git Resource", func() {
 				It("should detect non-existing revision", func() {
 					testRepo := func(repo string) {
 						withTempDir(func(target string) {
-							err := run(
+							err := run(withArgs(
 								"--url", repo,
 								"--target", target,
 								"--revision", "non-existent",
-							)
+							))
 
 							Expect(err).ToNot(BeNil())
 
@@ -593,11 +634,11 @@ var _ = Describe("Git Resource", func() {
 								GinkgoWriter.Write([]byte(sshPrivateKey))
 								file(filepath.Join(secret, "ssh-privatekey"), 0400, []byte(sshPrivateKey))
 
-								err := run(
+								err := run(withArgs(
 									"--url", repo,
 									"--target", target,
 									"--secret-path", secret,
-								)
+								))
 
 								Expect(err).ToNot(BeNil())
 
