9312d8c2a80800f06c62949adbe4619cf6d65d58
Sascha Schwarze <schwarzs@de.ibm.com>
2023-07-26 21:10:07 +0200

    Setup security context of controller deployment, enforce restricted PodSecurity in namespace

--- hack/install-kind.sh
+++ hack/install-kind.sh
@@ -13,7 +13,7 @@ set -eu
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 # kind version
-KIND_VERSION="${KIND_VERSION:-v0.17.0}"
+KIND_VERSION="${KIND_VERSION:-v0.20.0}"
 
 if ! hash kind > /dev/null 2>&1 ; then
     echo "# Installing KinD..."
@@ -27,7 +27,7 @@ kind --version
 KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-kind}"
 
 # kind cluster version
-KIND_CLUSTER_VERSION="${KIND_CLUSTER_VERSION:-v1.25.3}"
+KIND_CLUSTER_VERSION="${KIND_CLUSTER_VERSION:-v1.25.11}"
 
 echo "# Creating a new Kubernetes cluster..."
 kind delete cluster --name="${KIND_CLUSTER_NAME}"
