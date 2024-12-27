7969040a5d836ea8bdea8955b44ea5ec3d08aec7
Sascha Schwarze <schwarzs@de.ibm.com>
2024-12-27 16:47:07 +0100

    Test connectivity to the Git or Registry host before accessing the source
    This aims to mitigate rare failures where a BuildRun pod runs in a namespace with NetworkPolicies present. Kubernetes defines that isolation rules are guaranteed to be applied before any container of the Pod is started, but allow rules are eventually applied.
    
    Signed-off-by: Sascha Schwarze <schwarzs@de.ibm.com>

--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -16,6 +16,27 @@ import (
 
 var _ = Describe("Git", func() {
 
+	DescribeTable("the extraction of hostname and port",
+		func(url string, expectedHost string, expectedPort int, expectError bool) {
+			host, port, err := git.ExtractHostnamePort(url)
+			if expectError {
+				Expect(err).To(HaveOccurred())
+			} else {
+				Expect(err).ToNot(HaveOccurred())
+				Expect(host).To(Equal(expectedHost), "for "+url)
+				Expect(port).To(Equal(expectedPort), "for "+url)
+			}
+		},
+		Entry("Check heritage SSH URL with default port", "ssh://github.com/shipwright-io/build.git", "github.com", 22, false),
+		Entry("Check heritage SSH URL with custom port", "ssh://github.com:12134/shipwright-io/build.git", "github.com", 12134, false),
+		Entry("Check SSH URL with default port", "git@github.com:shipwright-io/build.git", "github.com", 22, false),
+		Entry("Check HTTP URL with default port", "http://github.com/shipwright-io/build.git", "github.com", 80, false),
+		Entry("Check HTTPS URL with default port", "https://github.com/shipwright-io/build.git", "github.com", 443, false),
+		Entry("Check HTTPS URL with custom port", "https://github.com:9443/shipwright-io/build.git", "github.com", 9443, false),
+		Entry("Check HTTPS URL with credentials", "https://somebody:password@github.com/shipwright-io/build.git", "github.com", 443, false),
+		Entry("Check invalid URL", "ftp://github.com/shipwright-io/build", "", 0, true),
+	)
+
 	DescribeTable("the source url validation errors",
 		func(url string, expected types.GomegaMatcher) {
 			Expect(git.ValidateGitURLExists(context.TODO(), url)).To(expected)
