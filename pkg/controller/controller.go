12cbd8b3e92212f88433d033f9f602c8ca2482e7
Sascha Schwarze <schwarzs@de.ibm.com>
2025-04-17 21:23:29 +0200

    Only cache pods and taskruns with the buildrun label
    Co-authored-by: Benjamin Isinger <benjamin.isinger@ibm.com>
    Signed-off-by: Sascha Schwarze <schwarzs@de.ibm.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -7,12 +7,18 @@ package controller
 import (
 	"context"
 
-	pipelineapi "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
-
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
+	pipelineapi "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+
 	"github.com/shipwright-io/build/pkg/apis"
+	buildv1beta1 "github.com/shipwright-io/build/pkg/apis/build/v1beta1"
 	"github.com/shipwright-io/build/pkg/config"
 	"github.com/shipwright-io/build/pkg/ctxlog"
 	"github.com/shipwright-io/build/pkg/reconciler/build"
@@ -25,6 +31,35 @@ import (
 
 // NewManager add all the controllers to the manager and register the required schemes
 func NewManager(ctx context.Context, config *config.Config, cfg *rest.Config, options manager.Options) (manager.Manager, error) {
+	// Setup a scheme
+	options.Scheme = k8sruntime.NewScheme()
+	if err := corev1.AddToScheme(options.Scheme); err != nil {
+		return nil, err
+	}
+	if err := pipelineapi.AddToScheme(options.Scheme); err != nil {
+		return nil, err
+	}
+	if err := apis.AddToScheme(options.Scheme); err != nil {
+		return nil, err
+	}
+
+	// Configure the cache
+	buildRunLabelExistsSelector, err := labels.Parse(buildv1beta1.LabelBuildRun)
+	if err != nil {
+		return nil, err
+	}
+
+	options.Cache = cache.Options{
+		ByObject: map[client.Object]cache.ByObject{
+			&corev1.Pod{}: {
+				Label: buildRunLabelExistsSelector,
+			},
+			&pipelineapi.TaskRun{}: {
+				Label: buildRunLabelExistsSelector,
+			},
+		},
+	}
+
 	mgr, err := manager.New(cfg, options)
 	if err != nil {
 		return nil, err
@@ -38,15 +73,6 @@ func NewManager(ctx context.Context, config *config.Config, cfg *rest.Config, op
 	}
 
 	ctxlog.Info(ctx, "Registering Components.")
-
-	if err := pipelineapi.AddToScheme(mgr.GetScheme()); err != nil {
-		return nil, err
-	}
-
-	// Setup Scheme for all resources
-	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		return nil, err
-	}
 
 	// Add Reconcilers.
 	if err := build.Add(ctx, config, mgr); err != nil {
