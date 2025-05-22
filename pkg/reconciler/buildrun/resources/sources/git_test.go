24dace0c02575683963219e0b43e8dfa1db2ab6f
ayushsatyam146 <ayushsatyam146@gmail.com>
2025-05-22 18:36:30 +0530

    feat: add depth parameter in git source of v1beta1 API
    Signed-off-by: ayushsatyam146 <ayushsatyam146@gmail.com>

--- pkg/reconciler/buildrun/resources/sources/git_test.go
+++ pkg/reconciler/buildrun/resources/sources/git_test.go
@@ -21,7 +21,6 @@ var _ = Describe("Git", func() {
 	cfg := config.NewDefaultConfig()
 
 	Context("when adding a public Git source", func() {
-
 		var taskSpec *pipelineapi.TaskSpec
 
 		BeforeEach(func() {
@@ -59,7 +58,6 @@ var _ = Describe("Git", func() {
 	})
 
 	Context("when adding a private Git source", func() {
-
 		var taskSpec *pipelineapi.TaskSpec
 
 		BeforeEach(func() {
@@ -106,6 +104,100 @@ var _ = Describe("Git", func() {
 			Expect(taskSpec.Steps[0].VolumeMounts[0].Name).To(Equal("shp-a-secret"))
 			Expect(taskSpec.Steps[0].VolumeMounts[0].MountPath).To(Equal("/workspace/shp-source-secret"))
 			Expect(taskSpec.Steps[0].VolumeMounts[0].ReadOnly).To(BeTrue())
+		})
+	})
+
+	Context("when adding a Git source with a depth parameter", func() {
+		var taskSpec *pipelineapi.TaskSpec
+
+		BeforeEach(func() {
+			taskSpec = &pipelineapi.TaskSpec{}
+		})
+
+		It("adds --depth argument with value 0 when depth is set to 0", func() {
+			depth := 0
+			sources.AppendGitStep(cfg, taskSpec, buildv1beta1.Git{
+				URL:   "https://github.com/shipwright-io/build",
+				Depth: ptr.To(depth),
+			}, "default")
+
+			Expect(len(taskSpec.Steps)).To(Equal(1))
+			// Check specific arguments for --depth 0
+			Expect(taskSpec.Steps[0].Args).To(ContainElement("--depth"))
+			Expect(taskSpec.Steps[0].Args).To(ContainElement("0"))
+			// Ensure other necessary args are still present
+			Expect(taskSpec.Steps[0].Args).To(ContainElement("--url"))
+		})
+
+		It("adds --depth argument with value 1 when depth is set to 1", func() {
+			depth := 1
+			sources.AppendGitStep(cfg, taskSpec, buildv1beta1.Git{
+				URL:   "https://github.com/shipwright-io/build",
+				Depth: ptr.To(depth),
+			}, "default")
+
+			Expect(len(taskSpec.Steps)).To(Equal(1))
+			Expect(taskSpec.Steps[0].Args).To(ContainElements("--depth", "1"))
+		})
+
+		It("adds --depth argument with specified value when depth is greater than 1", func() {
+			depth := 5
+			sources.AppendGitStep(cfg, taskSpec, buildv1beta1.Git{
+				URL:   "https://github.com/shipwright-io/build",
+				Depth: ptr.To(depth),
+			}, "default")
+
+			Expect(len(taskSpec.Steps)).To(Equal(1))
+			Expect(taskSpec.Steps[0].Args).To(ContainElements("--depth", "5"))
+		})
+
+		It("does not add --depth argument when source.Depth is nil (not specified)", func() {
+			sources.AppendGitStep(cfg, taskSpec, buildv1beta1.Git{
+				URL: "https://github.com/shipwright-io/build",
+			}, "default")
+
+			Expect(len(taskSpec.Steps)).To(Equal(1))
+			Expect(taskSpec.Steps[0].Args).NotTo(ContainElement("--depth"))
+		})
+
+		It("combines depth with other Git parameters", func() {
+			depth := 3
+			revision := "main"
+			sources.AppendGitStep(cfg, taskSpec, buildv1beta1.Git{
+				URL:         "https://github.com/shipwright-io/build",
+				Depth:       ptr.To(depth),
+				Revision:    ptr.To(revision),
+				CloneSecret: ptr.To("a.secret"),
+			}, "default")
+
+			Expect(len(taskSpec.Steps)).To(Equal(1))
+			Expect(taskSpec.Steps[0].Args).To(ContainElements(
+				"--url", "https://github.com/shipwright-io/build",
+				"--revision", "main",
+				"--depth", "3",
+				"--secret-path", "/workspace/shp-source-secret",
+			))
+		})
+
+		It("correctly passes --depth 0 when combined with other Git parameters", func() {
+			depth := 0
+			revision := "develop"
+			sources.AppendGitStep(cfg, taskSpec, buildv1beta1.Git{
+				URL:         "https://github.com/shipwright-io/another-repo",
+				Depth:       ptr.To(depth),
+				Revision:    ptr.To(revision),
+				CloneSecret: ptr.To("another.secret"),
+			}, "default")
+		
+			Expect(len(taskSpec.Steps)).To(Equal(1))
+			Expect(taskSpec.Steps[0].Args).To(ContainElements(
+				"--url", "https://github.com/shipwright-io/another-repo",
+				"--revision", "develop",
+				"--depth", "0",
+				"--secret-path", "/workspace/shp-source-secret",
+			))
+			Expect(taskSpec.Volumes).To(ContainElement(HaveField("Name", "shp-another-secret")))
+			Expect(taskSpec.Steps[0].VolumeMounts).To(ContainElement(HaveField("Name", "shp-another-secret")))
 		})
 	})
 })
