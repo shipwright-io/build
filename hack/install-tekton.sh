78af5bb7bb28baa96351ad7e2940715cff429106
Sascha Schwarze <schwarzs@de.ibm.com>
2026-09-14 17:12:41 +0200

    Allow image pulling to be retried
    Signed-off-by: Sascha Schwarze <schwarzs@de.ibm.com>

--- hack/install-tekton.sh
+++ hack/install-tekton.sh
@@ -17,3 +17,6 @@ TEKTON_HOST_PATH="tektoncd/pipeline/releases/download"
 echo "# Deploying Tekton Pipelines '${TEKTON_VERSION}'"
 
 kubectl apply -f "https://${TEKTON_HOST}/${TEKTON_HOST_PATH}/${TEKTON_VERSION}/release.yaml"
+
+# Allow image pulling to be retried (https://github.com/tektoncd/pipeline/pull/7666/files)
+kubectl -n tekton-pipelines patch configmap config-defaults -p '{"data":{"default-imagepullbackoff-timeout":"10m"}}'
